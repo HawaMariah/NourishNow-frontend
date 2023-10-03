b1b521ead8ff2a67eb2313317d524718c4cee425
Hawa <hawamaria33@gmail.com>
2023-10-03 12:09:54 +0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaHandHoldingDroplet } from 'react-icons/fa';
+
+
+function Navbar() {
+  return (
+    <>
+    <div>Navbar </div>
+    <FaHandHoldingDroplet />
+    </>
+    
+  )
+}
+
+export default Navbar
+
