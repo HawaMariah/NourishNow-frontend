825c09b077a269a36dc931084e12e9e8aca50eab
barondevke <ndabasteve1@gmail.com>
2023-10-04 11:56:49 +0300

    added new files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FAQ from './components/FAQ'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+  <FAQ/>
+
+    </>
+  )
+}
+
+export default App
