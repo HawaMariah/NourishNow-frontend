15b882950cbbea3800db8c2357bf0f5437d3c62a
barondevke <ndabasteve1@gmail.com>
2023-10-04 13:07:00 +0300

    completed page

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,37 +2,46 @@ import React from 'react'
 
 function FAQ() {
   return (
-    <div>
-       <h1 class="mb-2 mt-0 text-5xl font-medium leading-tight text-primary">
-  FAQs
-</h1>
+    <div >
+     
+ <section style={{backgroundColor:'#7E8F90'}} class="mb-24 text-center">
+    <div class="flex justify-start object-fill">
+      <div class="">
+        <h2 style={{fontColor:"white"}} class="mb-12 ml-2 mt-2 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl">
+          FAQs 
+           </h2>
+           
+       
+      </div>
+    </div>
+  </section>
 
-<div class="container my-24 mx-auto md:px-6">
+
+<div class="container mt-2 my-24 mx-auto md:px-3">
   
   <section class="mb-32">
-    <div class="grid gap-4 md:grid-cols-2">
-      <div class="mb-6 md:mb-0">
-        <h2 class="mb-6 text-3xl font-bold">Frequently asked questions</h2>
+    <div class="grid gap-4 md:grid-cols-3">
+      <div style={{backgroundColor:'#7E8F90'}} class="mb-6 md:mb-0">
+        <h2 class="mb-16 mt-20 ml-4 text-4xl font-bold ">DELIVERY</h2>
+        <h2 class="mb-16 ml-4 text-4xl  font-bold">ACCOUNT</h2>
+        <h2 class="mb-16 ml-4 text-4xl font-bold">FIND US</h2>
+        <h2 class="mb-16 ml-4 text-4xl font-bold">JOIN US</h2>
+        <h2 class="mb-16 ml-4 text-4xl font-bold">REGISTRATION</h2>
 
-        <p class="text-neutral-500 dark:text-neutral-300">
-          Didn't find your answer in the FAQ?
-          <a href="#!"
-            class="text-primary transition duration-300 hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600">Contact
-            our sales team</a>.
-        </p>
+
       </div>
 
-      <div class="mb-6 md:mb-0">
-        <p class="mb-4 font-bold">Anim pariatur cliche reprehenderit?</p>
-        <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+      <div class="mb-6 md:mb-0 col-span-2">
+        <p class="mb-4 mt-16 font-bold text-black text-3xl">Anim pariatur cliche reprehenderit?</p>
+        <p class="mb-12 text-black ">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
           autem numquam dolore molestias aperiam culpa alias veritatis
           architecto eos, molestiae vitae ex eligendi libero eveniet
           dolorem, doloremque rem aliquid perferendis.
         </p>
 
-        <p class="mb-4 font-bold">Non cupidatat skateboard dolor brunch?</p>
-        <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+        <p class="mb-4 font-bold text-black text-3xl">Non cupidatat skateboard dolor brunch?</p>
+        <p class="mb-12 text-black ">
           Distinctio corporis, iure facere ducimus quos consectetur ipsa ut
           magnam autem doloremque ex! Id, sequi. Voluptatum magnam sed fugit
           iusto minus et suscipit? Minima sunt at nulla tenetur, numquam
@@ -40,10 +49,10 @@ function FAQ() {
           libero repellendus cupiditate mollitia quidem dolorem odit
         </p>
 
-        <p class="mb-4 font-bold">
+        <p class="mb-4 font-bold text-black text-3xl">
           Praesentium voluptatibus temporibus consequatur non aspernatur?
         </p>
-        <p class="mb-12 text-neutral-500 dark:text-neutral-300">
+        <p class="mb-12 text-black">
           Minima sunt at nulla tenetur, numquam unde quod modi magnam ab
           deserunt ipsam sint aliquid dolores libero repellendus cupiditate
           mollitia quidem dolorem.
